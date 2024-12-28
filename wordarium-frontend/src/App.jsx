3efa122f3320e1b45e2432fac20957f42c736d17
Bhanu Pratap Singh <nikrcnkhil@gmail.com>
2024-12-28 21:40:24 +0530

    Completed Project

--- wordarium-frontend/src/App.jsx
+++ wordarium-frontend/src/App.jsx
@@ -6,19 +6,14 @@ import ContactUs from "./pages/ContactUs";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import { AuthContext } from "./context/authContext";
-import Register from "./components/register";
 import SeeAllBlogs from "./components/SeeAllBlogs";
+import Register from "./components/register";
 
 const App = () => {
     const { auth, panel, setPanel } = useContext(AuthContext);
     const access = auth?.token ? true : false;
   return (
     <div className="relative w-screen h-screen font-[Satoshi]">
-    {panel && !access && (
-        <div className="fixed top-0 left-0 w-screen h-screen z-50 flex items-center justify-center bg-black bg-opacity-50">
-          <Register panelFunc={setPanel} panelVal={panel} />
-        </div>
-      )}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
